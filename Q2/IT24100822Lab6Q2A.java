da93258d3e2bdba3b18422d24756f2cddc98a695
IT24100822 <IT24100822@my.sliit.lk>
2024-09-02 21:52:26 +0530

    Add IT24100822Lab6Q2A.java to Q2

---
+++ Q2/IT24100822Lab6Q2A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class IT24100822Lab6Q2A {
+	public static void main(String[] args)
+      {
+	int Number = 1;	
+		while (Number<=10) 
+            {
+			System.out.print(Number+" ");
+			Number=Number+1;
+		}
+	}
+}
